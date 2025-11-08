68eab19b85ce634e0ac853101c560a74a949f213
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-11-08 19:29:18 +0900

    Testing font import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import "./globals.css";
+import { Dela_Gothic_One, Zen_Kaku_Gothic_New } from 'next/font/google'
 import Link from "next/link";
+
+const delaGothicOne = Dela_Gothic_One({
+  weight: ['400']
+})
+
+const zenKaku = Zen_Kaku_Gothic_New({
+  weight: ["400", "300", "500", "700", "900"]
+})
 
 export default function RootLayout({
   children,
@@ -7,7 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={zenKaku.className}
+    >
       <body className="bg-darkcoal text-yukiwhite">
         <nav className="flex flex-row content-center justify-end h-16">
           <Link className="basis-32" href="/">stuart yukinaga</Link>
